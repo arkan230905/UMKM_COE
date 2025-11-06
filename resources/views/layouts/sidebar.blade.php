2276663d8d71cce9076c331fcd47a447e02c44c5
Arkan <arkan230905@gmail.com>
2025-11-06 11:09:53 +0700

    Reorder sidebar menus per requested structure; add Jabatan routes and links

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -13,27 +13,28 @@
         <li class="nav-item mt-3">
             <div class="text-uppercase text-muted small mb-2">Master Data</div>
         </li>
-        <li class="nav-item"><a href="{{ url('/master-data/aset') }}" class="nav-link"><i class="bi bi-box2-heart me-2"></i> Aset</a></li>
+        <li class="nav-item"><a href="{{ route('master-data.coa.index') }}" class="nav-link"><i class="bi bi-list-check me-2"></i> COA</a></li>
+        <li class="nav-item"><a href="{{ route('master-data.aset.index') }}" class="nav-link"><i class="bi bi-box2-heart me-2"></i> Aset</a></li>
+        <li class="nav-item"><a href="{{ route('master-data.jabatan.index') }}" class="nav-link"><i class="bi bi-person-badge me-2"></i> Jabatan</a></li>
         <li class="nav-item"><a href="{{ route('master-data.pegawai.index') }}" class="nav-link"><i class="bi bi-people-fill me-2"></i> Pegawai</a></li>
         <li class="nav-item"><a href="{{ route('master-data.presensi.index') }}" class="nav-link"><i class="bi bi-calendar-check me-2"></i> Presensi</a></li>
-        <li class="nav-item"><a href="{{ route('master-data.produk.index') }}" class="nav-link"><i class="bi bi-box-seam me-2"></i> Produk</a></li>
         <li class="nav-item"><a href="{{ route('master-data.vendor.index') }}" class="nav-link"><i class="bi bi-truck me-2"></i> Vendor</a></li>
-        <li class="nav-item"><a href="{{ route('master-data.bahan-baku.index') }}" class="nav-link"><i class="bi bi-basket3 me-2"></i> Bahan Baku</a></li>
         <li class="nav-item"><a href="{{ route('master-data.satuan.index') }}" class="nav-link"><i class="bi bi-upc-scan me-2"></i> Satuan</a></li>
-        <li class="nav-item"><a href="{{ route('master-data.coa.index') }}" class="nav-link"><i class="bi bi-list-check me-2"></i> COA</a></li>
+        <li class="nav-item"><a href="{{ route('master-data.produk.index') }}" class="nav-link"><i class="bi bi-box-seam me-2"></i> Produk</a></li>
+        <li class="nav-item"><a href="{{ route('master-data.bahan-baku.index') }}" class="nav-link"><i class="bi bi-basket3 me-2"></i> Bahan Baku</a></li>
         <li class="nav-item"><a href="{{ route('master-data.bop-budget.index') }}" class="nav-link"><i class="bi bi-calculator me-2"></i> BOP</a></li>
         <li class="nav-item"><a href="{{ route('master-data.bom.index') }}" class="nav-link"><i class="bi bi-diagram-3 me-2"></i> BOM</a></li>
 
         <li class="nav-item mt-3">
             <div class="text-uppercase text-muted small mb-2">Transaksi</div>
         </li>
-        <li class="nav-item"><a href="{{ route('transaksi.produksi.index') }}" class="nav-link"><i class="bi bi-diagram-3 me-2"></i> Produksi</a></li>
         <li class="nav-item"><a href="{{ route('transaksi.pembelian.index') }}" class="nav-link"><i class="bi bi-cart me-2"></i> Pembelian</a></li>
+        <li class="nav-item"><a href="{{ route('transaksi.produksi.index') }}" class="nav-link"><i class="bi bi-diagram-3 me-2"></i> Produksi</a></li>
         <li class="nav-item"><a href="{{ route('transaksi.penjualan.index') }}" class="nav-link"><i class="bi bi-currency-dollar me-2"></i> Penjualan</a></li>
         <li class="nav-item"><a href="{{ route('transaksi.retur.index') }}" class="nav-link"><i class="bi bi-arrow-counterclockwise me-2"></i> Retur</a></li>
-        <li class="nav-item"><a href="{{ route('transaksi.penggajian.index') }}" class="nav-link"><i class="bi bi-wallet2 me-2"></i> Penggajian</a></li>
         <li class="nav-item"><a href="{{ route('transaksi.expense-payment.index') }}" class="nav-link"><i class="bi bi-cash-coin me-2"></i> Pembayaran Beban</a></li>
         <li class="nav-item"><a href="{{ route('transaksi.ap-settlement.index') }}" class="nav-link"><i class="bi bi-credit-card me-2"></i> Pelunasan Utang</a></li>
+        <li class="nav-item"><a href="{{ route('transaksi.penggajian.index') }}" class="nav-link"><i class="bi bi-wallet2 me-2"></i> Penggajian</a></li>
 
         <li class="nav-item mt-3">
             <div class="text-uppercase text-muted small mb-2">Laporan</div>
