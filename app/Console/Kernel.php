2bef56032107b0f311224bf4a0ffcb2a39ae5e32
Arkan <arkan230905@gmail.com>
2026-05-04 03:18:15 +0700

    Add CreateTestSale command to test journal functionality
    - Create CreateTestSale artisan command to create test sale with journal entries
    - Verify that journal umum functionality works correctly
    - Create complete sale with HPP journal entries
    - This will help test if jurnal umum displays sale entries properly
    
    Usage: php artisan create:test-sale --user=4

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\DebugExactJurnalQuery::class,
         \App\Console\Commands\DebugJurnalFilter::class,
         \App\Console\Commands\FindSaleUser::class,
+        \App\Console\Commands\CreateTestSale::class,
     ];
 
     /**
