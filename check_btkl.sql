ce3280dcf3e41786dd91729fe03c7bdeca0cf0f3
Arkan <arkan230905@gmail.com>
2026-05-13 14:13:53 +0700

    Chore: Update .gitignore dan bersihkan file backup dari repositori

--- check_btkl.sql
+++
@@ -1,3 +0,0 @@
-SELECT b.id, b.kode_proses, b.nama_btkl, b.tarif_per_jam as tarif_stored, b.kapasitas_per_jam,
-       ROUND(b.tarif_per_jam / b.kapasitas_per_jam, 0) as biaya_per_produk
-FROM btkls b WHERE b.kapasitas_per_jam > 0;
--- check_pembayaran_beban.sql
+++
@@ -1,9 +0,0 @@
--- Check pembayaran beban records
-SELECT 
-    id,
-    tanggal,
-    beban_operasional_id,
-    jumlah,
-    keterangan
-FROM pembayaran_bebans
-ORDER BY tanggal;
