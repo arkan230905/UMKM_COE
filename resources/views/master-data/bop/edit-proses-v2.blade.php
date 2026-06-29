3b18bda9a347f6bbdac2cbaeab62ef77491f7494
Arkan <arkan230905@gmail.com>
2026-06-30 06:57:55 +0700

    Remove duplicate Bahan Pendukung section header in edit-proses-v2

--- resources/views/master-data/bop/edit-proses-v2.blade.php
+++ resources/views/master-data/bop/edit-proses-v2.blade.php
@@ -75,13 +75,6 @@
                 <!-- Bahan Pendukung -->
                 <div class="row g-3 mb-4">
                     <div class="col-12">
-                        <h5 class="mb-3">
-                            <i class="fas fa-boxes me-2"></i>Bahan Pendukung
-                        </h5>
-                        <small class="text-muted">Pilih bahan pendukung yang digunakan dalam proses produksi</small>
-                    </div>
-
-                    <div class="col-12">
                         <div class="table-responsive">
                             <table class="table table-bordered table-hover" id="bahanPendukungTable">
                                 <thead>
